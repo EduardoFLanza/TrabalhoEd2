272db7187189f2b153c812f81065efd9df4701c2
EduardoFLanza <eduardo.flanza@uel.br>
2025-10-06 10:15:41 -0300

    Update mapa.h
    exemplo

--- mapa.h
+++ mapa.h
@@ -1,1 +1,23 @@
 /* "enunciado do mapa aviário */
+
+
+/* "Modelo"
+
+  Atributo Descrição
+Vertices
+id Um identificador (string)
+x,y Uma coordenada do segmento de rua
+
+Arestas
+nome Nome da rua a qual pertence o segmento
+ldir Cep da quadra que está do lado direito do segmento de rua
+lesq Idem para o lado esquerdo
+cmp comprimento (em metros) do segmento de rua
+vm Velocidade média (m/s) que os carros trafegam neste
+
+segmento de rua
+
+
+
+
+*/
