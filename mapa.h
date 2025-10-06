1fc4ca3ceb13d5b1faf646a1da26d4008da091df
EduardoFLanza <eduardo.flanza@uel.br>
2025-10-06 10:23:33 -0300

    Update mapa.h
    n sei onde botar um negócio, ent vai ficar aqui até eu me organizar

--- mapa.h
+++ mapa.h
@@ -18,6 +18,23 @@ vm Velocidade média (m/s) que os carros trafegam neste
 segmento de rua
 
 
+Não sei onde botar isso ainda, ent vai ficar aqui**********************************************************************************
+
+<path d="M10,110 A120,120 -45 0,1 110 10 A120,120 -45 0,1 10,110"
+stroke="lightgrey" stroke-width="2" fill="none" id="theMotionPath"/>
+<path d="m 32.539299,36.987292 -1.233889,70.589218 50.211412,0.87769 0.458653,-26.239002
+-28.795792,-0.503346 0.345472,-19.763936 60.819315,1.063112 -0.40189,22.991614 -13.58416,-0.23745
+-0.36862,21.088178 49.3119,0.86196 1.16814,-66.828091 -54.976546,-0.960982" id="path10" />
+<circle cx="" cy="" r="5" fill="red">
+<animateMotion dur="6s" repeatCount="indefinite">
+<mpath xlink:href="#theMotionPath"/>
+</animateMotion>
+</circle>
+<rect x="" y="" width="5" height="5" fill="blue">
+<animateMotion dur="6s" repeatCount="indefinite">
+<mpath xlink:href="#path10"/>
+</animateMotion>
+</rect>
 
 
 */
