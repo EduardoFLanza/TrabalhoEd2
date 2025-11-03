f5231b6ffe00f8fc1a69b5d082afb8df76b0b13a
EduardoFLanza <eduardo.flanza@uel.br>
2025-11-03 10:22:30 -0300

    Update digraph.c
    teste de implementação de digraph.c

--- digraph.c
+++ digraph.c
@@ -1,171 +1,266 @@
-/*fazer implementação de digraph (grafo direcionado)*/
+/* --- Funções do grafo --- */
 
 /*
-Um Grafo direcionado  G e' constituido por um conjunto de vertices V e 
-um conjunto de arestas E, denotado por G=(V,E). 
-
-Pode-se associar um dado a arestas e a vertices de G.
-A cada vertice e' associado um nome.
-
-Existem tambem operacoes que possibilitam percorrer o grafo,
-invocando funcoes quando arestas do grafo 
-sao "descobertas", podendo, inclusive, interromper o percurso.
-
-Este modulo faz parte de um trabalho de Estrutura de Dados do 
-curso de Ciencia da Computacao da Universidade Estadual de Londrina.
-E' expressamente proibido o uso de ferramentas de IA para a sua implementacao.
-A documentacao deste modulo deve ser melhorada.
-*/
-
-
-typedef void *Graph;
-typedef int Node;
-typedef void *Edge;
-typedef void *Info;
-
-/*
-  Invocado quando uma aresta é "descoberta"/"percorrida"/"classificada". 
-  Tambem informa os tempos de descoberta e finalizacao
+ * createGraph()
+ * -------------
+ * Cria um grafo direcionado com capacidade máxima de nVert nós.
+ * Inicializa o número de nós atuais como 0 e aloca memória para os nós.
  */
-bool (*procEdge)(g,e,td,tf, void *extra); 
+Graph createGraph(int nVert) {
+    GraphStruct *g = (GraphStruct*) malloc(sizeof(GraphStruct));
+    if (!g) return NULL;
+
+    g->maxNodes = nVert;
+    g->totalNodes = 0;
+
+    g->nodes = (NodeStruct*) malloc(nVert * sizeof(NodeStruct));
+    if (!g->nodes) {
+        free(g);
+        return NULL;
+    }
+
+    for (int i = 0; i < nVert; i++)
+        g->nodes[i].adj.inicio = NULL;
+
+    return (Graph) g;
+}
 
 /*
-  Invocado quando percurso e' recomecado
+ * getMaxNodes()
+ * -------------
+ * Retorna o número máximo de nós que o grafo pode ter.
  */
-bool (*dfsRestarted)(g, void *extra);
-
-
+int getMaxNodes(Graph g) {
+    GraphStruct *gr = (GraphStruct*) g;
+    return gr->maxNodes;
+}
 
 /*
-    Cria um grafo com, no maximo, "nVert" vertices.
+ * getTotalNodes()
+ * ---------------
+ * Retorna o número atual de nós adicionados no grafo.
  */
-Graph createGraph(nVert);
-
+int getTotalNodes(Graph g) {
+    GraphStruct *gr = (GraphStruct*) g;
+    return gr->totalNodes;
+}
 
 /*
-    Retorna numero maximo de vertices que grafo g pode conter.
+ * addNode()
+ * ---------
+ * Adiciona um novo nó ao grafo com o nome e informação fornecidos.
+ * Retorna o índice do nó adicionado ou -1 caso o grafo esteja cheio.
  */
-int getMaxNodes(Graph g);
+Node addNode(Graph g, const char *nome, Info info) {
+    GraphStruct *gr = (GraphStruct*) g;
+    if (gr->totalNodes >= gr->maxNodes) return -1;
 
+    Node n = gr->totalNodes;
+    gr->nodes[n].nome = strdup(nome);
+    gr->nodes[n].info = info;
+    gr->nodes[n].adj.inicio = NULL;
+    gr->totalNodes++;
+
+    return n;
+}
 
 /*
-    Retorna numero total de vertices adicionados ao grafo gr.
+ * getNode()
+ * ---------
+ * Busca um nó pelo seu nome.
+ * Retorna o índice do nó ou -1 caso não exista.
  */
-int getTotalNodes(Graph g);
-
+Node getNode(Graph g, const char *nome) {
+    GraphStruct *gr = (GraphStruct*) g;
+    for (int i = 0; i < gr->totalNodes; i++)
+        if (strcmp(gr->nodes[i].nome, nome) == 0)
+            return i;
+    return -1;
+}
 
 /*
-    Adiciona um novo vértice ao grafo "g" com o nome "nome".
+ * getNodeInfo()
+ * -------------
+ * Retorna a informação associada a um nó específico.
  */
-Node addNode(g, nome, info);
-
+Info getNodeInfo(Graph g, Node node) {
+    GraphStruct *gr = (GraphStruct*) g;
+    return gr->nodes[node].info;
+}
 
 /*
-    Retorna no' cujo de nome e' "nome". 
+ * getNodeName()
+ * -------------
+ * Retorna o nome do nó especificado pelo índice.
  */
-Node getNode(g, nome);
-
+char *getNodeName(Graph g, Node node) {
+    GraphStruct *gr = (GraphStruct*) g;
+    return gr->nodes[node].nome;
+}
 
 /*
+ * setNodeInfo()
+ * -------------
+ * Atualiza a informação de um nó específico.
  */
-Info getNodeInfo(g, node);
+void setNodeInfo(Graph g, Node node, Info info) {
+    GraphStruct *gr = (GraphStruct*) g;
+    gr->nodes[node].info = info;
+}
 
+/* --- Funções de arestas --- */
 
 /*
+ * addEdge()
+ * ---------
+ * Adiciona uma aresta direcionada do nó 'from' para o nó 'to', com a informação associada.
+ * Retorna o ponteiro para a aresta criada (Edge) ou NULL em caso de falha.
  */
-char *getNodeName(g, node);
+Edge addEdge(Graph g, Node from, Node to, Info info) {
+    GraphStruct *gr = (GraphStruct*) g;
+    Celula *nova = (Celula*) malloc(sizeof(Celula));
+    if (!nova) return NULL;
 
+    nova->to = to;
+    nova->info = info;
+    nova->prox = gr->nodes[from].adj.inicio;
+    gr->nodes[from].adj.inicio = nova;
+
+    return nova;
+}
 
 /*
+ * getEdge()
+ * ---------
+ * Retorna a aresta que conecta o nó 'from' ao nó 'to'.
+ * Retorna NULL se não existir.
  */
-void setNodeInfo(g, node, info);
-
+Edge getEdge(Graph g, Node from, Node to) {
+    GraphStruct *gr = (GraphStruct*) g;
+    Celula *aux = gr->nodes[from].adj.inicio;
+    while (aux != NULL) {
+        if (aux->to == to)
+            return aux;
+        aux = aux->prox;
+    }
+    return NULL;
+}
 
 /*
+ * getFromNode()
+ * -------------
+ * Dado um ponteiro de aresta, retorna o nó de origem (from).
  */
-Edge addEdge(g, from, to, info);
-
+Node getFromNode(Graph g, Edge e) {
+    Celula *c = (Celula*) e;
+    GraphStruct *gr = (GraphStruct*) g;
+    for (int i = 0; i < gr->totalNodes; i++) {
+        Celula *aux = gr->nodes[i].adj.inicio;
+        while (aux) {
+            if (aux == c) return i;
+            aux = aux->prox;
+        }
+    }
+    return -1;
+}
 
 /*
+ * getToNode()
+ * -----------
+ * Retorna o nó de destino (to) da aresta especificada.
  */
-Edge getEdge(g, from, to);
-
+Node getToNode(Graph g, Edge e) {
+    Celula *c = (Celula*) e;
+    return c->to;
+}
 
 /*
+ * getEdgeInfo()
+ * -------------
+ * Retorna a informação associada a uma aresta.
  */
-Node getFromNode(g, e)
-
-  
-/*
- */  
-Node getToNode(g, e);
-
+Info getEdgeInfo(Graph g, Edge e) {
+    Celula *c = (Celula*) e;
+    return c->info;
+}
 
 /*
+ * setEdgeInfo()
+ * -------------
+ * Atualiza a informação de uma aresta.
  */
-Info getEdgeInfo(g, e);
-
-
-/*
- */
-void setEdgeInfo(g, e, info);
-
+void setEdgeInfo(Graph g, Edge e, Info info) {
+    Celula *c = (Celula*) e;
+    c->info = info;
+}
 
 /*
+ * removeEdge()
+ * ------------
+ * Remove uma aresta do grafo, liberando a memória associada.
  */
-void removeEdge(g,e);
+void removeEdge(Graph g, Edge e) {
+    GraphStruct *gr = (GraphStruct*) g;
+    Node from = getFromNode(g, e);
+    Celula *prev = NULL;
+    Celula *cur = gr->nodes[from].adj.inicio;
 
+    while (cur) {
+        if (cur == e) {
+            if (prev)
+                prev->prox = cur->prox;
+            else
+                gr->nodes[from].adj.inicio = cur->prox;
+            free(cur);
+            return;
+        }
+        prev = cur;
+        cur = cur->prox;
+    }
+}
 
 /*
+ * isAdjacent()
+ * ------------
+ * Verifica se existe uma aresta do nó 'from' para o nó 'to'.
+ * Retorna true se existir, false caso contrário.
  */
-bool isAdjacent(Graph g, Node from, Node to);
+bool isAdjacent(Graph g, Node from, Node to) {
+    return getEdge(g, from, to) != NULL;
+}
 
-
-/* 
-   Adiciona 'a lista "nosAdjacentes" os nos adjacentes 'a "node".
- */
-void adjacentNodes(g, node, nosAdjacentes);
-
+/* --- Funções de listas --- */
 
 /*
-   Adiciona 'a lista "arestaAdjacentes" as arestas (x,y), tal que,
-   x == node.
+ * adjacentNodes()
+ * ---------------
+ * Retorna a lista de nós adjacentes a um dado nó.
+ * A lista é representada pelo ponteiro para a lista interna de adjacência.
  */
-void adjacentEdges(g, node, Lista arestasAdjacentes);
-
+void adjacentNodes(Graph g, Node node, Lista *nosAdjacentes) {
+    GraphStruct *gr = (GraphStruct*) g;
+    nosAdjacentes->inicio = gr->nodes[node].adj.inicio;
+}
 
 /*
-   Insere na lista "nomesNodes" os nomes atribuidos aos nos do grafo.
+ * killDG()
+ * --------
+ * Libera toda a memória alocada para o grafo, incluindo nós, nomes e arestas.
  */
-void  getNodeNames(Graph g, Lista nomesNodes);
+void killDG(Graph g) {
+    GraphStruct *gr = (GraphStruct*) g;
 
+    for (int i = 0; i < gr->totalNodes; i++) {
+        Celula *aux = gr->nodes[i].adj.inicio;
+        while (aux != NULL) {
+            Celula *tmp = aux;
+            aux = aux->prox;
+            free(tmp);
+        }
+        free(gr->nodes[i].nome);
+    }
 
-/*
-   Insere na lista "arestas", as arestas de g.
- */
-void getEdges(g, Lista arestas);
+    free(gr->nodes);
+    free(gr);
+}
 
-
-/*
-   Faz percurso em profundidade sobre  g, a partir do no' node, classificando 
-   as arestas do grafo, invocando a respectiva funcao.
-      A busca em profundidade, eventualmente, pode produzir uma floresta.
-   newTree e' invocada sempre que o percurso for retomado.
- */  
-bool dfs(g, node, procEdge treeEdge, forwardEdge, returnEdge,
-	 crossEdge, newTree, void *extra);
-
-
-/*
-   Percorre o grafo g em largura, a partir do no' node. discoverNode e' usada
-   para a aresta (x,y) usada para "descobrir" o y.
- */
-bool bfs(g, node, discoverNode, void *extra);
-
-
-/*
-   Destroi o grafo "g".
- */
-void killDG(Graph g);
-
-#endif
