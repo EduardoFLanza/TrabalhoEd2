d8424535dad0b7d8ec6845e621bf6878840c952c
EduardoFLanza <eduardo.flanza@uel.br>
2025-12-16 15:08:59 -0300

    Update Quadras.c

--- Quadras.c
+++ Quadras.c
@@ -1,27 +1,24 @@
 #include "quadras.h"
 #include "fileManager.h"
-#include "geometry.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/**
+/*
  * Estrutura interna que representa uma quadra urbana.
  */
 typedef struct QuadraStr {
-    char* id;
-
+    char *id;
     double x, y;
     double width, height;
-
-    char* sw;
-    char* cfill;
-    char* cstrk;
+    char *sw;
+    char *cfill;
+    char *cstrk;
     double opacidade;
 } QuadraStr;
 
-/**
+/*
  * Estrutura principal do TAD Quadras.
  */
 typedef struct QuadrasStr {
@@ -30,45 +27,53 @@ typedef struct QuadrasStr {
     STreap streap;
 } QuadrasStr;
 
-/* Função auxiliar para liberação de uma quadra */
-static void freeQuadra(Quadra q, void* aux)
+/*
+ * Função auxiliar para liberação de uma quadra.
+ */
+static void freeQuadra(Quadra q, void *aux)
 {
-    QuadraStr* quadra = (QuadraStr*) q;
+    QuadraStr *quadra = (QuadraStr *) q;
 
     free(quadra->id);
+    free(quadra->sw);
     free(quadra->cfill);
     free(quadra->cstrk);
-    free(quadra->sw);
     free(quadra);
 }
 
-Quadras processGeoFile(const char* path)
+/*
+ * Processa arquivo .geo e cria o conjunto de quadras.
+ */
+Quadras processGeoFile(const char *path)
 {
     if (!strstr(path, ".geo")) {
         printf("Arquivo inválido: %s\n", path);
         return NULL;
     }
 
-    FILE* f = fopen(path, "r");
+    FILE *f = fopen(path, "r");
     if (!f) return NULL;
 
-    QuadrasStr* qs = malloc(sizeof(QuadrasStr));
+    QuadrasStr *qs = malloc(sizeof(QuadrasStr));
     qs->nQuadras = 0;
     qs->tabelaHash = criaHash(50, true, 0.75f);
     qs->streap = st_create(1e-7);
 
     char op[32], id[64];
-    char sw[32], cfill[32], cstrk[32];
+    char sw[32] = "", cfill[32] = "", cstrk[32] = "";
     double x, y, w, h;
 
-    while (fscanf(f, "%s", op) == 1) {
-        if (!strcmp(op, "cq")) {
+    while (fscanf(f, "%s", op) == 1)
+    {
+        if (strcmp(op, "cq") == 0)
+        {
             fscanf(f, "%s %s %s", sw, cfill, cstrk);
         }
-        else if (!strcmp(op, "q")) {
+        else if (strcmp(op, "q") == 0)
+        {
             fscanf(f, "%s %lf %lf %lf %lf", id, &x, &y, &w, &h);
 
-            QuadraStr* q = malloc(sizeof(QuadraStr));
+            QuadraStr *q = malloc(sizeof(QuadraStr));
             q->id = strdup(id);
             q->x = x;
             q->y = y;
@@ -89,64 +94,78 @@ Quadras processGeoFile(const char* path)
     return qs;
 }
 
-void percorrerQuadras(Quadras quadras, FvisitaQuadra f, void* aux)
+/*
+ * Percorre todas as quadras.
+ */
+void percorrerQuadras(Quadras quadras, FvisitaQuadra f, void *aux)
 {
     if (!quadras || !f) return;
-    percursoLargura(((QuadrasStr*)quadras)->streap, (FvisitaNo)f, aux);
+    percursoLargura(((QuadrasStr *) quadras)->streap,
+                     (FvisitaNo) f, aux);
 }
 
+/*
+ * Obtém quadras em uma região retangular.
+ */
 void getQuadrasRegion(
     Quadras quadras,
     double x, double y,
     double w, double h,
-    Lista resultado
-){
+    Lista resultado)
+{
     if (!quadras) return;
-    getNodeRegiaoSTrp(((QuadrasStr*)quadras)->streap, x, y, w, h, resultado);
+    getNodeRegiaoSTrp(((QuadrasStr *) quadras)->streap,
+                      x, y, w, h, resultado);
 }
 
-Quadra getQuadraByID(Quadras quadras, const char* id)
+/*
+ * Busca quadra pelo identificador.
+ */
+Quadra getQuadraByID(Quadras quadras, const char *id)
 {
-    return getHashValue(((QuadrasStr*)quadras)->tabelaHash, id);
+    return getHashValue(((QuadrasStr *) quadras)->tabelaHash, id);
 }
 
 /* ===== GETTERS ===== */
 
-const char* getQuadraID(Quadra q){ return ((QuadraStr*)q)->id; }
-double getQuadraX(Quadra q){ return ((QuadraStr*)q)->x; }
-double getQuadraY(Quadra q){ return ((QuadraStr*)q)->y; }
-double getQuadraWidth(Quadra q){ return ((QuadraStr*)q)->width; }
-double getQuadraHeight(Quadra q){ return ((QuadraStr*)q)->height; }
-const char* getQuadraCFill(Quadra q){ return ((QuadraStr*)q)->cfill; }
-const char* getQuadraCStrk(Quadra q){ return ((QuadraStr*)q)->cstrk; }
-const char* getQuadraSW(Quadra q){ return ((QuadraStr*)q)->sw; }
-double getQuadraOpacidade(Quadra q){ return ((QuadraStr*)q)->opacidade; }
+const char *getQuadraID(Quadra q)       { return ((QuadraStr *) q)->id; }
+double getQuadraX(Quadra q)             { return ((QuadraStr *) q)->x; }
+double getQuadraY(Quadra q)             { return ((QuadraStr *) q)->y; }
+double getQuadraWidth(Quadra q)         { return ((QuadraStr *) q)->width; }
+double getQuadraHeight(Quadra q)        { return ((QuadraStr *) q)->height; }
+const char *getQuadraCFill(Quadra q)    { return ((QuadraStr *) q)->cfill; }
+const char *getQuadraCStrk(Quadra q)    { return ((QuadraStr *) q)->cstrk; }
+const char *getQuadraSW(Quadra q)       { return ((QuadraStr *) q)->sw; }
+double getQuadraOpacidade(Quadra q)     { return ((QuadraStr *) q)->opacidade; }
 
 /* ===== SETTERS ===== */
 
-void setQuadraCFill(Quadra q, const char* cfill)
+void setQuadraCFill(Quadra q, const char *cfill)
 {
-    QuadraStr* quadra = (QuadraStr*)q;
+    QuadraStr *quadra = (QuadraStr *) q;
     free(quadra->cfill);
     quadra->cfill = strdup(cfill);
 }
 
-void setQuadraCStrk(Quadra q, const char* cstrk)
+void setQuadraCStrk(Quadra q, const char *cstrk)
 {
-    QuadraStr* quadra = (QuadraStr*)q;
+    QuadraStr *quadra = (QuadraStr *) q;
     free(quadra->cstrk);
     quadra->cstrk = strdup(cstrk);
 }
 
 void setQuadraOpacidade(Quadra q, double opacidade)
 {
-    ((QuadraStr*)q)->opacidade = opacidade;
+    ((QuadraStr *) q)->opacidade = opacidade;
 }
 
+/*
+ * Remove uma quadra do sistema.
+ */
 void removerQuadra(Quadras quadras, Quadra q)
 {
-    QuadrasStr* qs = (QuadrasStr*)quadras;
-    QuadraStr* quadra = (QuadraStr*)q;
+    QuadrasStr *qs = (QuadrasStr *) quadras;
+    QuadraStr  *quadra = (QuadraStr *) q;
 
     SNode n = st_search(qs->streap, quadra->x, quadra->y);
     if (n) deleteNodeSTrp(qs->streap, n);
@@ -155,14 +174,20 @@ void removerQuadra(Quadras quadras, Quadra q)
     freeQuadra(q, NULL);
 }
 
+/*
+ * Retorna a STreap interna.
+ */
 STreap getQuadrasSTrp(Quadras quadras)
 {
-    return ((QuadrasStr*)quadras)->streap;
+    return ((QuadrasStr *) quadras)->streap;
 }
 
-void freeQuadras(Quadras quadras, void* aux)
+/*
+ * Libera todas as quadras.
+ */
+void freeQuadras(Quadras quadras, void *aux)
 {
-    QuadrasStr* qs = (QuadrasStr*)quadras;
+    QuadrasStr *qs = (QuadrasStr *) quadras;
     st_destroy(qs->streap, freeQuadra);
     destroiHash(qs->tabelaHash, NULL, NULL);
     free(qs);
